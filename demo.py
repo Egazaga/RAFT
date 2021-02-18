926117ee0693713b37070300ece327707d035bce
egazaga <zg2000@yandex.ru>
2021-02-19 02:34:07 +0300

    Changed inference

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('core')
 
 import argparse
@@ -13,9 +14,8 @@ from raft import RAFT
 from utils import flow_viz
 from utils.utils import InputPadder
 
-
-
 DEVICE = 'cuda'
+
 
 def load_image(imfile):
     img = np.array(Image.open(imfile)).astype(np.uint8)
@@ -23,10 +23,10 @@ def load_image(imfile):
     return img[None].to(DEVICE)
 
 
-def viz(img, flo):
-    img = img[0].permute(1,2,0).cpu().numpy()
-    flo = flo[0].permute(1,2,0).cpu().numpy()
-    
+def convert(img, flo):
+    img = img[0].permute(1, 2, 0).cpu().numpy()
+    flo = flo[0].permute(1, 2, 0).cpu().numpy()
+
     # map flow to rgb image
     flo = flow_viz.flow_to_image(flo)
     img_flo = np.concatenate([img, flo], axis=0)
@@ -35,11 +35,18 @@ def viz(img, flo):
     # plt.imshow(img_flo / 255.0)
     # plt.show()
 
-    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
-    cv2.waitKey()
+    return img_flo[:, :, [2, 1, 0]] / 255.0
 
 
-def demo(args):
+def infer_raft(in_path, out_path):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default='./models/raft-things.pth', help="restore checkpoint")
+    parser.add_argument('--path', default=in_path, help="dataset for evaluation")
+    parser.add_argument('--small', action='store_true', default=False, help='use small model')
+    parser.add_argument('--mixed_precision', action='store_true', default=False, help='use mixed precision')
+    parser.add_argument('--alternate_corr', action='store_true', default=False, help='use efficent correlation implementation')
+    args = parser.parse_args()
+
     model = torch.nn.DataParallel(RAFT(args))
     model.load_state_dict(torch.load(args.model))
 
@@ -50,9 +57,9 @@ def demo(args):
     with torch.no_grad():
         images = glob.glob(os.path.join(args.path, '*.png')) + \
                  glob.glob(os.path.join(args.path, '*.jpg'))
-        
+
         images = sorted(images)
-        for imfile1, imfile2 in zip(images[:-1], images[1:]):
+        for i, imfile1, imfile2 in enumerate(zip(images[:-1], images[1:])):
             image1 = load_image(imfile1)
             image2 = load_image(imfile2)
 
@@ -60,16 +67,8 @@ def demo(args):
             image1, image2 = padder.pad(image1, image2)
 
             flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-            viz(image1, flow_up)
+            cv2.imwrite(out_path + str(i).zfill(6) + ".png", convert(image1, flow_up))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help="restore checkpoint")
-    parser.add_argument('--path', help="dataset for evaluation")
-    parser.add_argument('--small', action='store_true', help='use small model')
-    parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
-    parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
-    args = parser.parse_args()
-
-    demo(args)
+    infer_raft(in_path="in", out_path="out")
